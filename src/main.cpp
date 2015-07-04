15480097276f456f3556030a18eb7bd978e7ca6f
Brian Wilson <polytopes@gmail.com>
2015-07-04 10:19:11 -0400

    Complete working *old* OpenGL code

--- src/main.cpp
+++ src/main.cpp
@@ -6,54 +6,83 @@
 
 void ErrorCallback(int error, const char *description){
   std::cerr << description << std::endl;
-  glfwTerminate();
-  exit( -1 );
 }
 
 static void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mods){
-  std::cout << "I'm here!\n";
   if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
     glfwSetWindowShouldClose( window, GL_TRUE );
 }
 
 int main(int argc, char **argv){
 
-  int errorCode = EXIT_FAILURE;
+  GLFWwindow *window;
   
-  if (!glfwInit())
-    exit( errorCode );
-
+  // Set up error handling
   glfwSetErrorCallback( ErrorCallback );
 
-  GLFWwindow *window = glfwCreateWindow( 640, 480, "Sample OpenGL", NULL, NULL );
-  if (!window) goto EXIT;
+  // Initialize GLFW
+  if (!glfwInit())
+    exit( EXIT_FAILURE );
 
 
-  glfwMakeContextCurrent( window );
-
-  glfwSetKeyCallback( window, KeyCallback );
-
-
-  // Rendering
-  int width, height;
-  glfwGetFramebufferSize( window, &width, &height );
-  glViewport( 0, 0, width, height );
-  glfwSwapInterval( 1 );
-
-
-  // Main loop (for now)
-  while (!glfwWindowShouldClose( window )){
-    double t = glfwGetTime();
-
-    glfwSwapBuffers( window );
-    glfwPollEvents();
-
+  // Create our rendering window
+  window = glfwCreateWindow( 640, 480, "Sample OpenGL", NULL, NULL );
+  if (!window) {
+    glfwTerminate();
+    exit( EXIT_FAILURE );
   }
 
 
-  errorCode = EXIT_SUCCESS;
+  glfwMakeContextCurrent( window ); // Set OpenGL rendering context
+  glfwSwapInterval( 1 );            // Set swap interval to 1 to avoid tearing
 
- EXIT:
+  // Handle keypresses
+  glfwSetKeyCallback( window, KeyCallback );
+
+
+  // Main loop
+  while (!glfwWindowShouldClose( window )){
+    float ratio;
+    int width, height;
+
+    glfwGetFramebufferSize( window, &width, &height );
+    ratio = width / (float) height;
+
+    // Set the displayed viewport and clear the background
+    glViewport( 0, 0, width, height );
+    glClear( GL_COLOR_BUFFER_BIT );
+
+
+    // Render time!
+
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glOrtho( -ratio, ratio, -1.f, 1.f, 1.f, -1.f );
+    glMatrixMode( GL_MODELVIEW );
+    
+    glLoadIdentity();
+    glRotatef( (float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f );
+    
+    glBegin( GL_TRIANGLES );
+
+    glColor3f( 1.f, 0.f, 0.f );
+    glVertex3f( -0.6f, -0.4f, 0.f );
+    
+    glColor3f( 0.f, 1.f, 0.f );
+    glVertex3f( 0.6, -0.4f, 0.f );
+
+    glColor3f( 0.f, 0.f, 1.f );
+    glVertex3f( 0.f, 0.6f, 0.f );
+
+    glEnd();
+
+    glfwSwapBuffers( window );  // Display our new scene
+    glfwPollEvents();           // Handle event processing
+  }
+
+
+  // Clean up
+  glfwDestroyWindow( window );
   glfwTerminate();
-  exit( errorCode );
+  exit( EXIT_SUCCESS );
 }
